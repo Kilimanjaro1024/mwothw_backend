5929592dd9e1ceb456a39d9a9e9f6e3f2fb91d5c
Clayton Miller <clayton.miller@loop.colum.edu>
2021-03-02 15:25:12 -0600

    adds backend structure for creating an account an logging in.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,67 @@
+class UsersController < ApplicationController
+  before_action :authorized, only: [:auto_login]
+
+  # GET /users
+  def index
+    @users = User.all
+
+    render json: @users
+  end
+
+  # GET /users/1
+  def show
+    render json: @user
+  end
+
+  # Register
+  def create
+    @user = User.create(user_params)
+
+    if @user.valid?
+      token = encode_token({user_id: @user.id})
+      render json: {user: @user, token: token}
+    else
+      render json: {error: "Invalid username or passowrd"}
+    end
+  end
+
+  #Logging in
+  def login 
+    @user = User.find_by(username: params[:username])
+
+    if @user && @user.authenticate(params[:password])
+      token = encode_token({user_id: @user.id})
+      render json: {user: @user, token: token}
+    else
+      render json: {error: "Invalid username or password"}
+    end
+  end
+
+  def auto_login
+    render json: @user
+  end
+  # PATCH/PUT /users/1
+  def update
+    if @user.update(user_params)
+      render json: @user
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /users/1
+  def destroy
+    @user.destroy
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def user_params
+      params.permit(:username, :password, :email)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  resources :users, only: [:create]
+  post "/login", to: "user#login"
+  get "/auto_login", to: "user#auto_login"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
