1bcf3529469c511225e6319c266f07b825edd3b0
Clayton Miller <clayton.miller@loop.colum.edu>
2021-03-02 19:05:30 -0600

    Adds working login and registation backend

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :users, only: [:create]
-  post "/login", to: "user#login"
-  get "/auto_login", to: "user#auto_login"
+  post "/login", to: "users#login"
+  get "/auto_login", to: "users#auto_login"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
