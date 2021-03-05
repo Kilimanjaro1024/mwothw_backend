4dd836fbb109516876a5afacd3faf8b5a9b3e6d5
Clayton Miller <clayton.miller@loop.colum.edu>
2021-03-04 19:56:03 -0600

    adds functioning posts and user relationships

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,22 +1,32 @@
 class PostsController < ApplicationController
-  before_action :authorized, :set_post, only: [:show, :update, :destroy]
+  before_action :authorized
+  before_action :set_post, only: [:show, :update, :destroy]
 
   # GET /posts
   def index
-    @posts = Post.where(user_id: @user.id)
+    @posts = Post.all
 
     render json: @posts
   end
 
   # GET /posts/1
   def show
-    render json: @post
+    @posts = Post.where(user_id: @user.id)
+
+    render json: @posts
+  end
+
+  def user_posts
+    @posts = Post.where(user_id: @user.id)
+
+    render json: @posts
   end
 
   # POST /posts
   def create
     @post = Post.new(post_params)
     @post.user_id = @user.id
+    @post.username = @user.username
 
     if @post.save
       render json: @post, status: :created, location: @post
